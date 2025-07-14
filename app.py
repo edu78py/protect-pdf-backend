040be196c7150608305e26c339e4d687356f1d1e
edu78py <113313472+edu78py@users.noreply.github.com>
2025-07-13 21:05:41 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, send_file
+from flask_cors import CORS
+from pypdf import PdfReader, PdfWriter
+import io
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/proteger', methods=['POST'])
+def proteger():
+    file = request.files.get('file')
+    if not file:
+        return {'error': 'Archivo no recibido'}, 400
+
+    reader = PdfReader(file)
+    writer = PdfWriter()
+
+    for page in reader.pages:
+        writer.add_page(page)
+
+    writer.encrypt(user_password="", owner_password="1234", permissions={"modify": False, "copy": False})
+
+    output = io.BytesIO()
+    writer.write(output)
+    output.seek(0)
+
+    return send_file(output, as_attachment=True, download_name="protegido.pdf", mimetype="application/pdf")
+
+if __name__ == '__main__':
+    app.run(debug=True)
